1f81d30cfc7c6447bb5b20ad6953a76bd450e5bd
Berg40 <wandemberg28@gmain.com>
2025-11-27 10:23:26 -0300

    Versáo1.2.1

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'treinofacil-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './treino.html',
+  './calculadora.html',
+  './dieta.html',
+  './style.css',
+  './tmb.css',
+  './script.js',
+  './script_tmb.js',
+  '.img/logo_treino_facil.png',
+  '.style2.css',
+         // <--- ADICIONE ESTA LINHA (não esqueça a vírgula na linha anterior)
+  './manifest.json'    // <--- Importante salvar o manifesto também
+  
+  // Adicione aqui outras imagens ou arquivos que queira que funcionem offline
+];
+
+// Instalação: Salva os arquivos no cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ativação: Limpa caches antigos se houver atualização
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptação: Serve o arquivo do cache se estiver offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
